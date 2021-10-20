c3f6394f21d0cf6f1caddb0c4862fbeb3cc1d6b9
ingryong <bbgod2@gmail.com>
2021-10-20 16:18:46 +0900

    커리큘럼페이지 작업중

--- src/views/Creators/EditClass3.vue
+++ src/views/Creators/EditClass3.vue
@@ -9,6 +9,36 @@
           [저장하기]버튼을 누르면 모든 수정 사항은 즉시 반영되어 보여집니다.
         </p>
       </div>
+
+      <!-- 소개 -->
+      <div id="cur_area"></div>
+      <div class="input-group" v-for="(chap, num) in array1" :key="num">
+        <div class="cur_container">
+          <h4>
+            #{{ num + 1 }}
+            <input
+              type="text"
+              class="form-control m-1"
+              style="display:inline; width:90%;"
+              placeholder="챕터명을 입력해주세요"
+              :value="chap"
+            />
+          </h4>
+          <div class="cur_left">
+            <img src="@/assets/imgs/PhotoPotrait.svg" />
+          </div>
+          <div class="cur_right">
+            <input
+              v-for="(list, i) in array2"
+              :key="i"
+              type="text"
+              class="form-control m-1"
+              placeholder="챕터명을 입력해주세요"
+              :value="list"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +50,28 @@ export default {
       storage: this.$firebase.storage(),
       user: this.$store.state.user,
       url: this.$route.params.id,
-      doc: ""
+      doc: "",
+      img: "../../assets/imgs/PhotoPotrait.svg",
+      array1: [
+        "챕터1 입니다",
+        "챕터2 입니다",
+        "챕터3 입니다",
+        "챕터4 입니다",
+        "챕터5 입니다",
+        "챕터6 입니다"
+      ],
+      array2: [
+        ["aaa", "123", "333", "444", "14124"],
+        ["1sss11", "222", "333", "444"],
+        ["sdfsdfas", "2521344", "333433", "444"],
+        ["11sdfasd1", "222", "333", "444"],
+        ["1as11", "222", "333", "444"],
+        ["1aagwe11", "222", "333", "444"],
+        ["11q241", "222", "333", "444"],
+        ["14yrga11", "222", "333", "444"],
+        ["11jwsry1", "222", "333", "444"],
+        ["asdf", "222", "333", "444"]
+      ]
     };
   },
   mounted() {
