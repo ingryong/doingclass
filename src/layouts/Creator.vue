b3661a18b9a354e8f1b250341a51ef913ef8a5dc
ingryong <bbgod2@gmail.com>
2022-03-28 21:31:54 +0900

    creator-myclass 디자인 틀어짐 수정

--- src/layouts/Creator.vue
+++ src/layouts/Creator.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <CreatorNavbar></CreatorNavbar>
-    <div id="creators-left-side">
-      <h5>클래스 관리</h5>
-      <ul>
-        <li>
-          <router-link to="/creators/myclass">개설한 클래스 관리</router-link>
-        </li>
-      </ul>
-    </div>
     <router-view :key="$route.fullPath" />
   </div>
 </template>
@@ -24,20 +16,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#creators-left-side {
-  position: fixed;
-  top: 50px;
-  left: 0px;
-  z-index: 98;
-  width: 220px;
-  height: 100%;
-  border-right: 1px solid $gray-1;
-  box-sizing: border-box;
-  padding: 10px;
-  background-color: #fcfcfc;
-  ul {
-    padding: 10px;
-  }
-}
-</style>
+<style lang="scss"></style>
--- src/views/Creators/myClass.vue
+++ src/views/Creators/myClass.vue
@@ -1,45 +1,65 @@
 <template>
   <div class="myclass_container">
-    <div class="class_group">
-      <h4>개설한 클래스 관리</h4>
-      <router-link class="create_class_button" to="/creators/createclass"
-        >+ 새로운 클래스</router-link
-      >
-      <hr />
+    <div class="creators-left-side">
+      <h5>클래스 관리</h5>
+      <ul>
+        <li>
+          <router-link to="/creators/myclass">개설한 클래스 관리</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="card-body class_group">
+      <div class="d-flex justify-content-between">
+        <h4 class="p-2">개설한 클래스 관리</h4>
+        <router-link class="p-2" to="/creators/createclass">
+          + 새로운 클래스
+        </router-link>
+      </div>
       <!-- 개설된 클래스가 없을 경우 -->
       <div class="none_class" v-if="!classinfo">
         <h3>개설한 클래스가 없습니다.</h3>
       </div>
       <!-- 개설한 클래스 리스트 -->
       <div id="class_view">
-        <div class="class_list" v-for="list in classinfo" :key="list.id">
-          <div class="class_thumbnail">
+        <div
+          class="d-flex p-2 my-2 flex-row w-100 card-body border-0"
+          style="height:200px;"
+          v-for="list in classinfo"
+          :key="list.id"
+        >
+          <div class="class_thumbnail me-2">
             <img
               src="@/assets/imgs/PhotoPotrait.svg"
               v-if="list.thumbnail === ''"
             />
             <img :src="list.thumbnail" v-if="list.thumbnail" />
           </div>
-          <div class="class_info">
+          <div
+            class="class_info w-100 px-2 d-flex flex-column justify-content-between"
+          >
             <ul>
-              <li>
-                <span>{{ list.type }}</span> ·
-                <span>{{ list.category.c1 }}</span> ·
-                <span>{{ list.category.c2 }}</span>
+              <li class="class-title">{{ list.title }}</li>
+              <li class="class-category">
+                <span class="badge bg-secondary">{{ list.type }}</span> ·
+                <span class="badge bg-secondary">{{ list.category.c1 }}</span> ·
+                <span class="badge bg-secondary">{{ list.category.c2 }}</span>
               </li>
-              <li class="title">{{ list.title }}</li>
               <li v-if="list.classopen">
-                <span class="tag-open" style="margin:0;">클래스 오픈</span
-                ><br /><br />클래스가 오픈중입니다.
+                <span class="badge bg-info" style="margin:0;">
+                  클래스 오픈
+                </span>
               </li>
               <li v-if="!list.classopen">
-                <span class="tag-ready" style="margin:0;">오픈 준비중</span
-                ><br /><br />아직 클래스가 오픈되지 않았습니다.<br />추가 클래스
-                정보를 입력하고 클래스를 오픈하세요!
+                <span class="badge bg-secondary py-2" style="margin:0;">
+                  오픈 준비중
+                </span>
+                <br />
+                아직 클래스가 오픈되지 않았습니다. <br />
+                추가 클래스 정보를 입력하고 클래스를 오픈하세요!
               </li>
             </ul>
             <router-link
-              class="btn-l btn-gray class_setting_button"
+              class="btn btn-outline-dark w-100 py-2"
               :to="`/creators/editclass1/${list.id}`"
               >클래스 관리 <font-awesome-icon :icon="['fas', 'cog']" />
             </router-link>
@@ -79,16 +99,32 @@ export default {
 
 <style lang="scss">
 .myclass_container {
-  max-width: 700px;
-  padding: 10px;
-  margin-left: 240px;
+  max-width: 1000px;
+  min-width: 800px;
   margin-top: 70px;
 
+  .creators-left-side {
+    position: fixed;
+    z-index: 98;
+    top: 57px;
+    min-width: 240px;
+    height: 100%;
+    border-right: 1px solid $gray-1;
+    box-sizing: border-box;
+    padding: 10px;
+    padding-top: 30px;
+    background-color: #fcfcfc;
+
+    ul {
+      padding: 10px;
+    }
+  }
   .class_group {
-    width: 100%;
+    margin-left: 250px;
+    margin-top: 10px;
+    padding: 20px;
     border: 1px solid #eee;
     border-radius: 10px;
-    box-sizing: border-box;
 
     .none_class {
       margin: 40px;
@@ -96,61 +132,22 @@ export default {
       color: $gray-2;
     }
 
-    h4 {
-      box-sizing: border-box;
-      padding: 0px 20px;
-      border: none;
-      display: inline-block;
-    }
-
-    hr {
-      background-color: #eee;
-      height: 1px;
-      border: none;
-      margin-top: 0;
-    }
-
-    .create_class_button {
-      display: block;
-      float: right;
-      margin: 22px;
-      font-size: 0.9rem;
-      font-weight: bold;
-      color: $gray-3;
-      text-align: right;
-    }
-    a:hover {
-      color: $black;
-    }
-
-    .class_list {
-      display: flex;
-      flex-direction: row;
-      margin-top: 5px;
-
-      .class_thumbnail {
-        margin: auto;
-        width: 240px;
-        img {
-          width: 240px;
-          height: 181px;
-          border-radius: 4px;
-        }
+    .class_thumbnail {
+      margin: auto;
+      max-width: 240px;
+      img {
+        max-width: 240px;
+        max-height: 181px;
+        border-radius: 4px;
       }
-      .class_info {
-        margin: 0px 10px 10px auto;
-        width: 400px;
-        .title {
-          font-weight: bold;
-          font-size: 1.15rem;
-        }
-
-        .class_setting_button {
-          text-align: center;
-          color: #000;
-          border-color: #fff;
-          border-radius: 4px;
-        }
+    }
+    .class_info {
+      .class-category {
+        font-size: 0.75rem;
+      }
+      .class-title {
+        font-weight: bold;
+        font-size: 1.15rem;
       }
     }
   }
